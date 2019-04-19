ee031f3d0d63505059c931d55d392543b07f2afd
Lily <44213621+lilysimmons@users.noreply.github.com>
2019-04-18 21:08:43 -0400

    fixed server

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
+
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
-const session = require('express-session')
-const dbConnection = require('./client/server/database') 
-const SequelStore = require('sequelStore');
-const passport = require('./client/server/passport');
+const bodyParser = require('body-parser');
+// const morgan = require('morgan')
+
+const db = require("./models");
+const user = require("./routes/user-api-routes");
+const sequence = require("./routes/sequence-api-routes");
+// const session = require('express-session')
+// const dbConnection = require('./client/server/database') 
+// const SequelStore = require('sequelStore');
+// const passport = require('./client/server/passport');
 const app = express()
-const port = 3001;
-// Route requires
-const user = require('./client/server/routes/user')
+// const port = 3001;
+// const PORT = 3001;
+// // Route requires
+// const user = require('./client/server/routes/user')
 
 // MIDDLEWARE
-app.use(morgan('dev'))
+// app.use(morgan('dev'))
 app.use(
 	bodyParser.urlencoded({
 		extended: false
@@ -20,22 +25,19 @@ app.use(
 )
 app.use(bodyParser.json())
 
-// Sessions
-app.use(
-	session({
-		secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
-		store: new SequelStore ({ SequelStore: dbConnection }),
-		resave: false, //required
-		saveUninitialized: false //required
-	})
-)
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
 
-// Routes
-app.use('/user', user)
-
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+  // Add routes, both API
+	app.use(user);
+	app.use(sequence);
+  
+  const PORT = process.env.PORT || 3001;
+  
+  db.sequelize.sync({ force: true }).then(() => {
+	app.listen(PORT, function() {
+	  console.log(`🌎 ==> API server now on port ${PORT}!`);
+	});
+  });
